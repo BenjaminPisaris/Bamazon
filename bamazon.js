55defc4f423f563956314b5d51f69bc32907ed4e
Ben Pisaris <datcrisis@gmail.com>
2018-09-12 13:17:51 -0400

    woah

--- bamazon.js
+++ bamazon.js
@@ -11,17 +11,23 @@ var connection = mySQL.createConnection({
     database: "bamazon"
 });
 
+//creates connection with the data table
+connection.connect(function(err) {
+if (err) {
+    console.log(`error connecting: ${err}`);
+} loadTable();
+});
+
 //load product data or error
-
-
 var loadTable = function() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) {
-            console.error(`error connecting: ${err.stack}`);
+            throw err;
         } else {
             //function to load the product data goes here
             console.table(res);
         }
     })
 }
+
 
